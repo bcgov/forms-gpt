faf3007c15eafa126e3377dc1fa2756627d3a20e
RyanBirtch-aot <104386035+RyanBirtch-aot@users.noreply.github.com>
2024-05-16 15:29:19 -0700

    Update FormList.js
    Updated Toast Wording

--- frontend/src/components/FormList.js
+++ frontend/src/components/FormList.js
@@ -32,7 +32,7 @@ const FormList = () => {
         <>
             <Toast className='mb-4'  style={{ width: "100%" }} bg='danger'>
                 <Toast.Header closeButton={false}>
-                Welcome to FormsGPT. Your data will not persist between logins. Please export any forms you would like to save.
+                Welcome to FormsGPT. Please export any forms you would like to save, saved forms will be deleted if your browser storage is reset.
                 </Toast.Header>
             </Toast>
             <div className="m-3">
